30e93db28bb6d80d5ee660086e6502b078057e37
anushasingh0703 <122735664+anushasingh0703@users.noreply.github.com>
2023-04-26 01:30:24 +0530

    Add files via upload

---
+++ OR.c
@@ -0,0 +1,8 @@
+//Anusha Singh, PRN - 21070123019
+#include <stdio.h>
+int main()
+{
+	int a=6, b=14;
+	printf("The output of the Bitwise OR operator a|b is %d",a|b);
+	return 0;
+}
---
+++ XOR.c
@@ -0,0 +1,9 @@
+//Anusha Singh, PRN - 21070123019
+#include <stdio.h>
+int main()
+{
+	int a=6, b=14;
+	printf("The output of the Bitwise XOR operator a|b is %d",a^b);
+	return 0;
+
+}
